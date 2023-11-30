ae4ec9ac371ace05dddbfc31a21e08fe299632fa
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2023-11-30 23:17:12 +0200

    last-updates

--- js/contact_us.js
+++ js/contact_us.js
@@ -33,36 +33,36 @@ $(document).ready(function () {
 
     // form
 
-//    var form = document.getElementById("my-form");
-//    
-//    async function handleSubmit(event) {
-//        event.preventDefault();
-//        var status = document.getElementById("status");
-//        var data = new FormData(event.target);
-//        fetch(event.target.action, {
-//            method: form.method,
-//            body: data,
-//            headers: {
-//                'Accept': 'application/json'
-//            }
-//        }).then(response => {
-//            if (response.ok) {
-//            status.innerHTML = "Thanks!";
-//            form.reset()
-//            } else {
-//            response.json().then(data => {
-//                if (Object.hasOwn(data, 'errors')) {
-//                status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-//                } else {
-//                status.innerHTML = "Oops! There was a problem."
-//                }
-//            })
-//            }
-//        }).catch(error => {
-//            status.innerHTML = "Oops! There was a problem."
-//        });
-//    }
-//    form.addEventListener("submit", handleSubmit)
+    var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+        var status = document.getElementById("status");
+        var data = new FormData(event.target);
+        fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+            status.innerHTML = "Thanks!";
+            form.reset()
+            } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                status.innerHTML = "Oops! There was a problem."
+                }
+            })
+            }
+        }).catch(error => {
+            status.innerHTML = "Oops! There was a problem."
+        });
+    }
+    form.addEventListener("submit", handleSubmit)
 
 
 });
